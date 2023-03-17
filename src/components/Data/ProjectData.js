b7a74e5ea8eead8abb5ff85be79a2ad0203588b3
omkar <Omkar Joshi>
2023-03-17 13:55:50 +0530

    completed remaing changes

---
+++ src/components/Data/ProjectData.js
@@ -0,0 +1,23 @@
+const ProjectData =[
+    {
+        title: "Xmeme",
+        Date: "Aug, 2021",
+        info: "A meme straming web app where you can share your funny memes using link and from device also!!",
+        // liveLink: "https://csb-sgffhc.netlify.app/",
+        sourceLink: "https://github.com/joshi-omkar/xmeme"
+    },
+    {
+        title: "Do you know your Triangles ?",
+        Date: "Aug, 2021",
+        info: "Find the basic calculator for triangles and take a quiz to test your knowledge. Made in ReactJs",
+        liveLink: "https://csb-sgffhc.netlify.app/",
+        sourceLink: "https://github.com/joshi-omkar/fun-with-triangle"
+    },
+    {
+        title: "Do you know your Triangles ?",
+        Date: "Aug, 2021",
+        info: "Find the basic calculator for triangles and take a quiz to test your knowledge. Made in ReactJs",
+        liveLink: "https://csb-sgffhc.netlify.app/",
+        sourceLink: "https://github.com/joshi-omkar/fun-with-triangle"
+    },
+]
